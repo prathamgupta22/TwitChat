82a0ff83c188d919d2eae04f2efacc6220b2361e
prathamgupta22 <pratham.guptapg01@gmail.com>
2023-12-27 23:03:29 +0530

    someChanges

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true })); // to parse from data in the re
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://twit-chat.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
